0ebf322a531b263d28383392af1a86b42287bd6a
ros <rk65536@protonmail.com>
2020-05-18 15:21:22 +0100

    add working public request

--- src/App.js
+++ src/App.js
@@ -18,8 +18,13 @@ function App() {
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <span class="nav-link">
+                ADC Base: {process.env.REACT_APP_ADC_BASE}
+              </span>
             </li>
           </ul>
         </div>
--- src/components/public-endpoint.js
+++ src/components/public-endpoint.js
@@ -1,8 +1,32 @@
 
 import React from 'react';
 
+async function getJson(url) {
+    const respo = await fetch(url, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+
+    return respo.text();
+}
+
 export default class PublicEndpoint extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            response: "",
+        };
+
+        this.makeRequest = this.makeRequest.bind(this);
+    }
+
+    async makeRequest() {
+        const url = `${process.env.REACT_APP_ADC_BASE}${this.props.url}`;
+        const json = await getJson(url);
+        this.setState(state => ({ response: json }));
+    }
 
     render() {
         return (
@@ -11,11 +35,11 @@ export default class PublicEndpoint extends React.Component {
                     <span style={{"font-size":30}}>
                         GET <b>{this.props.url}</b>
                     </span>
-                    <button class="btn btn-primary">Request</button>
+                    <button class="btn btn-primary" onClick={this.makeRequest}>Request</button>
                 </div>
                 <div class="bg-light">
                     <b>Response:</b> <br/>
-                    <span>AA</span>
+                    <span>{this.state.response}</span>
                 </div>
             </div>
         );
