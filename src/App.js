ec66ece08fb5b0735e9a43b1bcbc5e6df5461fce
ros <rk65536@protonmail.com>
2020-05-19 16:14:46 +0100

    implement search type

--- src/App.js
+++ src/App.js
@@ -16,6 +16,10 @@ import PublicEndpoint from './components/public-endpoint.js';
 import GetEndpoint from './components/get-endpoint.js';
 import PostEndpoint from './components/post-endpoint.js';
 
+const REPERTOIRE_FIELDS = ["repertoire_id", "repertoire_name", "repertoire_description", "study.study_id", "study.study_title", "study.study_type.value", "study.study_type.id", "study.study_description", "study.inclusion_exclusion_criteria", "study.grants", "study.collected_by", "study.lab_name", "study.lab_address", "study.submitted_by", "study.pub_ids", "study.keywords_study", "subject.subject_id", "subject.synthetic", "subject.organism.value", "subject.organism.id", "subject.sex", "subject.age_min", "subject.age_max", "subject.age_unit.value", "subject.age_unit.id", "subject.age_event", "subject.age", "subject.ancestry_population", "subject.ethnicity", "subject.race", "subject.strain_name", "subject.linked_subjects", "subject.link_type", "subject.diagnosis.study_group_description", "subject.diagnosis.disease_diagnosis", "subject.diagnosis.disease_length", "subject.diagnosis.disease_stage", "subject.diagnosis.prior_therapies", "subject.diagnosis.immunogen", "subject.diagnosis.intervention", "subject.diagnosis.medical_history", "sample.sample_id", "sample.sample_type", "sample.tissue", "sample.anatomic_site", "sample.disease_state_sample", "sample.collection_time_point_relative", "sample.collection_time_point_reference", "sample.biomaterial_provider", "sample.tissue_processing", "sample.cell_subset.value", "sample.cell_subset.id", "sample.cell_phenotype", "sample.cell_species.value", "sample.cell_species.id", "sample.single_cell", "sample.cell_number", "sample.cells_per_reaction", "sample.cell_storage", "sample.cell_quality", "sample.cell_isolation", "sample.cell_processing_protocol", "sample.template_class", "sample.template_quality", "sample.template_amount", "sample.library_generation_method", "sample.library_generation_protocol", "sample.library_generation_kit_version", "sample.pcr_target.pcr_target_locus", "sample.pcr_target.forward_pcr_primer_target_location", "sample.pcr_target.reverse_pcr_primer_target_location", "sample.complete_sequences", "sample.physical_linkage", "sample.sequencing_run_id", "sample.total_reads_passing_qc_filter", "sample.sequencing_platform", "sample.sequencing_facility", "sample.sequencing_run_date", "sample.sequencing_kit", "sample.sequencing_files.file_type", "sample.sequencing_files.filename", "sample.sequencing_files.read_direction", "sample.sequencing_files.read_length", "sample.sequencing_files.paired_filename", "sample.sequencing_files.paired_read_direction", "sample.sequencing_files.paired_read_length", "data_processing.data_processing_id", "data_processing.primary_annotation", "data_processing.software_versions", "data_processing.paired_reads_assembly", "data_processing.quality_thresholds", "data_processing.primer_match_cutoffs", "data_processing.collapsing_method", "data_processing.data_processing_protocols", "data_processing.data_processing_files", "data_processing.germline_database", "data_processing.analysis_provenance_id"];
+
+const REARRANGEMENT_FIELDS = ["sequence_id", "sequence", "sequence_aa", "rev_comp", "productive", "vj_in_frame", "stop_codon", "complete_vdj", "locus", "v_call", "d_call", "d2_call", "j_call", "c_call", "sequence_alignment", "sequence_alignment_aa", "germline_alignment", "germline_alignment_aa", "junction", "junction_aa", "np1", "np1_aa", "np2", "np2_aa", "np3", "np3_aa", "cdr1", "cdr1_aa", "cdr2", "cdr2_aa", "cdr3", "cdr3_aa", "fwr1", "fwr1_aa", "fwr2", "fwr2_aa", "fwr3", "fwr3_aa", "fwr4", "fwr4_aa", "v_score", "v_identity", "v_support", "v_cigar", "d_score", "d_identity", "d_support", "d_cigar", "d2_score", "d2_identity", "d2_support", "d2_cigar", "j_score", "j_identity", "j_support", "j_cigar", "c_score", "c_identity", "c_support", "c_cigar", "v_sequence_start", "v_sequence_end", "v_germline_start", "v_germline_end", "v_alignment_start", "v_alignment_end", "d_sequence_start", "d_sequence_end", "d_germline_start", "d_germline_end", "d_alignment_start", "d_alignment_end", "d2_sequence_start", "d2_sequence_end", "d2_germline_start", "d2_germline_end", "d2_alignment_start", "d2_alignment_end", "j_sequence_start", "j_sequence_end", "j_germline_start", "j_germline_end", "j_alignment_start", "j_alignment_end", "cdr1_start", "cdr1_end", "cdr2_start", "cdr2_end", "cdr3_start", "cdr3_end", "fwr1_start", "fwr1_end", "fwr2_start", "fwr2_end", "fwr3_start", "fwr3_end", "fwr4_start", "fwr4_end", "v_sequence_alignment", "v_sequence_alignment_aa", "d_sequence_alignment", "d_sequence_alignment_aa", "d2_sequence_alignment", "d2_sequence_alignment_aa", "j_sequence_alignment", "j_sequence_alignment_aa", "c_sequence_alignment", "c_sequence_alignment_aa", "v_germline_alignment", "v_germline_alignment_aa", "d_germline_alignment", "d_germline_alignment_aa", "d2_germline_alignment", "d2_germline_alignment_aa", "j_germline_alignment", "j_germline_alignment_aa", "c_germline_alignment", "c_germline_alignment_aa", "junction_length", "junction_aa_length", "np1_length", "np2_length", "np3_length", "n1_length", "n2_length", "n3_length", "p3v_length", "p5d_length", "p3d_length", "p5d2_length", "p3d2_length", "p5j_length", "consensus_count", "duplicate_count", "cell_id", "clone_id", "rearrangement_id", "repertoire_id", "sample_processing_id", "data_processing_id", "germline_database"];
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -76,6 +80,8 @@ export default class App extends React.Component {
   }
 
   render() {
+    
+
     return (
       <KeycloakProvider 
         onTokens={this.saveTokens} keycloak={keycloak}
@@ -114,7 +120,20 @@ export default class App extends React.Component {
             <GetEndpoint url="/repertoire" token={this.state.token} responseField="Repertoire"/>
             <GetEndpoint url="/rearrangement" token={this.state.token} responseField="Rearrangement"/>
 
-            <PostEndpoint url="/repertoire" token={this.state.token} responseField="Repertoire" publicFields={this.state.repertoirePublic}/>
+            <PostEndpoint 
+              url="/repertoire" 
+              token={this.state.token} 
+              responseField="Repertoire" 
+              publicFields={this.state.repertoirePublic}
+              fields={REPERTOIRE_FIELDS}
+            />
+            <PostEndpoint 
+              url="/rearrangement" 
+              token={this.state.token} 
+              responseField="Rearrangement" 
+              publicFields={this.state.rearrangementPublic}
+              fields={REARRANGEMENT_FIELDS}
+            />
           </div>
 
           <ToastContainer
--- src/components/post-endpoint.js
+++ src/components/post-endpoint.js
@@ -12,17 +12,27 @@ export default class PostEndpoint extends React.Component {
             response: "",
             isPublic: false,
             limit: "",
-            from: ""
+            from: "",
+            searchType: true,
+            facet: "",
+            showRequest: false,
         };
 
         this.makeRequest = this.makeRequest.bind(this);
         this.buildRequest = this.buildRequest.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.facetsOptions = this.facetsOptions.bind(this);
     }
 
     buildRequest() {
         const ret = {};
-        if (this.state.isPublic) {
+
+        if (this.state.searchType && this.state.isPublic) {
             ret.fields = this.props.publicFields;
+        }
+
+        if (!this.state.searchType && this.state.facet !== "") {
+            ret.facets = this.state.facet
         }
 
         if (this.state.limit !== "") {
@@ -39,7 +49,7 @@ export default class PostEndpoint extends React.Component {
     }
 
     async makeRequest() {
-        this.setState({...this.state, response: ""});
+        this.setState({ ...this.state, response: "" });
 
         const url = `${process.env.REACT_APP_ADC_BASE_PATH}${this.props.url}`;
         const request = this.buildRequest();
@@ -47,53 +57,114 @@ export default class PostEndpoint extends React.Component {
             let json = await postProtectedJson(url, this.props.token, request);
             if (this.props.responseField in json) {
                 json = json[this.props.responseField];
+            } else if ("Facet" in json) {
+                json = json["Facet"];
             }
             this.setState({ ...this.state, response: json });
         });
     }
 
+    handleChange(val) {
+        this.setState({ ...this.state, searchType: val.target.value === "search" });
+    }
+
+    facetsOptions() {
+        const ret = [];
+
+        for (let field of this.props.fields) {
+            ret.push(<option>{field}</option>);
+        }
+
+        return ret;
+    }
+
     render() {
         return (
-            <Endpoint 
-                url={this.props.url} 
-                name={this.props.url} 
-                request={this.makeRequest} 
-                json={this.state.response} 
+            <Endpoint
+                url={this.props.url}
+                name={this.props.url}
+                request={this.makeRequest}
+                json={this.state.response}
                 method="POST"
                 requestDisabled={this.props.token === ""}
             >
                 <div class="w-100">
+
                     <div class="d-flex justify-content-between">
-                        <div class="form-check">
-                            <input name="isPublic" class="form-check-input" type="checkbox" value="" id="publicFields"
-                                checked={this.state.isPublic}
-                                onChange={e => this.setState({...this.state, isPublic: e.target.checked})}
-                                />
-                            <label class="form-check-label" for="publicFields">
-                                Public Fields?
-                            </label>
+
+                        <div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name={this.props.url}  value="search" onChange={this.handleChange} checked={this.state.searchType} />
+                                <label class="form-check-label">
+                                    Search
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name={this.props.url}  value="facets" onChange={this.handleChange} checked={!this.state.searchType}/>
+                                <label class="form-check-label">
+                                    Facets
+                                </label>
+                            </div>
                         </div>
+
+                        {
+                            this.state.searchType ?
+                                (
+                                    this.props.publicFields.length !== 0 && <div class="form-check">
+                                        <input name="isPublic" class="form-check-input" type="checkbox" value="" id="publicFields"
+                                            checked={this.state.isPublic}
+                                            onChange={e => this.setState({ ...this.state, isPublic: e.target.checked })}
+                                        />
+                                        <label class="form-check-label" for="publicFields">
+                                            Public Fields?
+                                        </label>
+                                    </div>
+                                ) :
+                                (
+                                    <div class="form-group d-flex">
+                                        <label for="facetsInput">Facets: </label>
+                                        <select class="form-control ml-1" style={{width: '15rem'}} id="facetsInput" onChange={e => this.setState({ ...this.state, facet: e.target.value })}>
+                                            <option value="">--</option>
+                                            {this.facetsOptions()}
+                                        </select>
+                                    </div>
+                                )
+                        }
 
                         <div class="form-group d-flex">
                             <label for="limitInput">Size: </label>
                             <input type="number" class="form-control ml-2" id="limitInput" min="0"
-                                value={this.state.limit} 
-                                onChange={(event) => this.setState({...this.state, limit: event.target.value})}
+                                style={{width: '5rem'}}
+                                value={this.state.limit}
+                                onChange={(event) => this.setState({ ...this.state, limit: event.target.value })}
                             />
                         </div>
 
                         <div class="form-group d-flex">
                             <label for="fromInput">From: </label>
                             <input type="number" class="form-control ml-2" id="fromInput" min="0"
-                                value={this.state.from} 
-                                onChange={(event) => this.setState({...this.state, from: event.target.value})}
+                                style={{width: '5rem'}}
+                                value={this.state.from}
+                                onChange={(event) => this.setState({ ...this.state, from: event.target.value })}
                             />
+                        </div>
+
+                        <div class="form-check">
+                            <input name="isPublic" class="form-check-input" type="checkbox" value="" id="requestShow"
+                                checked={this.state.showRequest}
+                                onChange={e => this.setState({ ...this.state, showRequest: e.target.checked })}
+                            />
+                            <label class="form-check-label" for="requestShow">
+                                Show Request?
+                            </label>
                         </div>
                     </div>
 
-                    <span class="text-left">
-                        <JSONTree data={this.buildRequest()} />
-                    </span>
+                    {this.state.showRequest && (
+                        <span class="text-left">
+                            <JSONTree data={this.buildRequest()} />
+                        </span>
+                    )}
                 </div>
             </Endpoint>
         );
